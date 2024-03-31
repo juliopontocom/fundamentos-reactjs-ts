a67dd37d3efc9f19901897e514c7acdf221b5f72
juliopontocom <146362643+juliopontocom@users.noreply.github.com>
2024-03-31 20:20:54 -0300

    mudando link da imagem

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css'
 export function Header(){
     return (
         <header className={styles.header}>
-            <img src="/src/assets/ignite-logo.svg" alt="ignite-logo" />
+            <img src="https://odevjulio.com.br/assets/ignite-logo.svg" alt="ignite-logo" />
         </header>
     );
 }
